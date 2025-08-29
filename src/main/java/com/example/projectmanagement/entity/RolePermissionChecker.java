0472d804b040d36be877eb44aedaed610ed021df
Ruchitha <ruchithareddynuhtula@gmail.com>
2025-08-29 11:57:09 +0530

    add changes

--- src/main/java/com/example/projectmanagement/entity/RolePermissionChecker.java
+++ src/main/java/com/example/projectmanagement/entity/RolePermissionChecker.java
@@ -5,19 +5,19 @@ import com.example.projectmanagement.entity.User.UserRole;
 public class RolePermissionChecker {
 
     public static boolean canUpdateTask(UserRole role) {
-        return role == UserRole.DEVELOPER || role == UserRole.ADMIN;
+        return role == UserRole.DEVELOPER || role == UserRole.Admin;
     }
 
     public static boolean canStartSprint(UserRole role) {
-        return role == UserRole.PRODUCT_OWNER || role == UserRole.SCRUM_MASTER || role == UserRole.ADMIN;
+        return role == UserRole.PRODUCT_OWNER || role == UserRole.SCRUM_MASTER || role == UserRole.Admin;
     }
 
     public static boolean canDeleteSprint(UserRole role) {
-        return role == UserRole.ADMIN;
+        return role == UserRole.Admin;
     }
 
     public static boolean canCreateSprint(UserRole role) {
-        return role == UserRole.PRODUCT_OWNER || role == UserRole.SCRUM_MASTER || role == UserRole.ADMIN;
+        return role == UserRole.PRODUCT_OWNER || role == UserRole.SCRUM_MASTER || role == UserRole.Admin;
     }
 
     // Add more methods as needed

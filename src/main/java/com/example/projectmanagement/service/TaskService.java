223678d2a31373132d25cf31df099776410531bc
Ajay <kkajaykumar0313@gmail.com>
2025-11-17 19:02:53 +0530

    merge issues resolved

--- src/main/java/com/example/projectmanagement/service/TaskService.java
+++ src/main/java/com/example/projectmanagement/service/TaskService.java
@@ -26,4 +26,6 @@ public interface TaskService {
     Page<TaskViewDto> searchTasksView(String title, Task.Priority priority, Long assigneeId, Pageable pageable);
     TaskCreateDto updateTask(Long id, TaskUpdateDto dto);
     List<TaskViewDto> getTasksByStoryNew(Long storyId);
+    List<TaskTimesheetDto> getTimesheetsTasksByAssignee(Long assigneeId);
+
 }
